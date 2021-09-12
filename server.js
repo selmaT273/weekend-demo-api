181de3a211a79d0d0d1f1e1763ef2efea08f8055
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-12 16:18:01 -0500

    gettting data back in console from movie api

--- server.js
+++ server.js
@@ -12,12 +12,28 @@ const app = express();
 // enable CORS
 app.use(cors());
 
+// routes
 app.get('/', (request, response) => {
   response.send('Home page');
 })
+
+app.get('/movies', getMovies);
 
 
 // start server
 app.listen(PORT, () => {
   console.log(`Server is started on ${PORT}`);
 });
+
+const axios = require('axios');
+
+async function getMovies(request, response) {
+  const results = await axios.get('https://api.themoviedb.org/3/search/movie', {
+    params: {
+      api_key: process.env.MOVIE_API_KEY,
+      query: 'Cedar Rapids',
+    }
+  });
+  console.log(results.data);
+  response.send('SENT SOMETHING?');
+}
